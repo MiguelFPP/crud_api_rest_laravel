f66b0f1b2d572098b0ee11a686b9e340ce760d62
MiguelFPP <84242304+MiguelFPP@users.noreply.github.com>
2022-08-09 18:29:11 -0500

    test for functions student controller

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -17,6 +17,6 @@ class Student extends Model
         'phone',
         'birthdate',
         'address',
-        'status'
+        'active'
     ];
 }
--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -2,21 +2,111 @@
 
 namespace Tests\Feature;
 
+use App\Models\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StudentTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_can_view_all_students()
     {
-        $response = $this->get('/');
+        $this->withoutExceptionHandling();
+        $response = $this->get('/api/students');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'identification',
+                    'name',
+                    'surname',
+                    'email',
+                    'phone',
+                    'birthdate',
+                    'address',
+                    'status',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+    }
+
+    public function test_can_view_a_student()
+    {
+        $this->withoutExceptionHandling();
+        $student = Student::factory()->create();
+        $response = $this->get('/api/students/' . $student->id);
+        $response->assertStatus(200);
+    }
+
+    public function test_can_create_a_student()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api/students', [
+            'identification' => 1312,
+            'name' => 'Juan',
+            'surname' => 'Perez',
+            'email' => 'hola@hola.com',
+            'phone' => '123456789',
+            'birthdate' => '2020-01-01',
+            'address' => 'Calle falsa 123',
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'id',
+            'identification',
+            'name',
+            'surname',
+            'email',
+            'phone',
+            'birthdate',
+            'address',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_can_update_a_student()
+    {
+        $this->withoutExceptionHandling();
+        $student = Student::factory()->create();
+        $response = $this->put('/api/students/' . $student->id, [
+            'identification' => 1312,
+            'name' => 'Juan',
+            'surname' => 'Perez',
+            'email' => 'hola@hola.com',
+            'phone' => '123456789',
+            'birthdate' => '2020-01-01',
+            'address' => 'Calle falsa 123',
+        ]);
 
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'id',
+            'identification',
+            'name',
+            'surname',
+            'email',
+            'phone',
+            'birthdate',
+            'address',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    public function test_can_delete_a_student()
+    {
+        $this->withoutExceptionHandling();
+        $student = Student::factory()->create();
+        $response = $this->delete('/api/students/' . $student->id);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'message' => 'Student deleted',
+        ]);
     }
 }
